a6cfb8861eb66f9b318213564d88575762391884
Phil Cable <pcable@wsu.edu>
2015-05-29 08:02:52 -0700

    Implement headers via theme hook

--- functions.php
+++ functions.php
@@ -1,12 +1,19 @@
 <?php
 
+/**
+ * Set up a theme hook for the site header.
+ */
+function cahnrswp_site_header() {
+	do_action( 'cahnrswp_site_header' );
+}
+
 add_action( 'wp_enqueue_scripts', 'extension_wp_enqueue_scripts', 21 );
 /**
  * Enqueue scripts and styles required for front end pageviews.
  */
 function extension_wp_enqueue_scripts() {
 	wp_dequeue_style( 'spine-theme-extra' );
-	wp_enqueue_script( 'extension-js', get_stylesheet_directory_uri() . '/js/extension.js', array( 'jquery' ), '' );
+	//wp_enqueue_script( 'extension-js', get_stylesheet_directory_uri() . '/js/extension.js', array( 'jquery' ), '' );
 }
 
 add_filter( 'spine_option_defaults', 'extension_spine_option_defaults' );
@@ -20,3 +27,32 @@ function extension_spine_option_defaults( $defaults ) {
 	$defaults['articletitle_header'] = true;
 	return $defaults;
 }
+
+add_action( 'cahnrswp_site_header', 'cahnrswp_default_header', 1 );
+/**
+ * Add the default header.
+ *
+ * Yep, this is verging close to spaghetti. But it opens up some possibilities
+ */
+function cahnrswp_default_header() {
+	get_template_part( 'parts/default-header' );
+}
+
+add_filter( 'mce_buttons_2', 'cahnrswp_add_tinymce_table_plugin' );
+/**
+ * Add Table controls to tinyMCE editor.
+ */
+function cahnrswp_add_tinymce_table_plugin( $buttons ) {
+   array_push( $buttons, 'table' );
+   return $buttons;
+}
+
+add_filter( 'mce_external_plugins', 'cahnrswp_register_tinymce_table_plugin' );
+/**
+ * Register the tinyMCE Table plugin.
+ */
+function cahnrswp_register_tinymce_table_plugin( $plugin_array ) {
+   $plugin_array['table'] = get_stylesheet_directory_uri() . '/tinymce/table-plugin.min.js';
+   return $plugin_array;
+}
+
--- parts/default-header.php
+++ parts/default-header.php
@@ -1,28 +1,5 @@
-<?php
-
-/**
- * Retrieve an array of values to be used in the header.
- *
- * site_name
- * site_tagline
- * page_title
- * post_title
- * section_title
- * subsection_title
- * posts_page_title
- * sup_header_default
- * sub_header_default
- * sup_header_alternate
- * sub_header_alternate
- */
-$spine_main_header_values = spine_get_main_header();
-
-if ( spine_get_option( 'main_header_show' ) == 'true' ) :
-
-?>
-<header class="main-header">
-
-	<div class="cahnrs-header-group">
+<?php $spine_main_header_values = spine_get_main_header(); ?>
+<div class="cahnrs-header-group">
 		<div id="cahnrs-heading">
 			<a href="http://cahnrs.wsu.edu/">CAHNRS</a>
 			<div class="quicklinks">
@@ -55,9 +32,4 @@ if ( spine_get_option( 'main_header_show' ) == 'true' ) :
 			</div>
 		</div><sup class="sup-header" data-section="<?php echo $spine_main_header_values['section_title']; ?>" data-pagetitle="<?php echo $spine_main_header_values['page_title']; ?>" data-posttitle="<?php echo $spine_main_header_values['post_title']; ?>" data-default="<?php echo esc_html($spine_main_header_values['sup_header_default']); ?>" data-alternate="<?php echo esc_html($spine_main_header_values['sup_header_alternate']); ?>"><span class="sup-header-default"><?php echo strip_tags( $spine_main_header_values['sup_header_default'], '<a>' ); ?></span></sup>
 	</div>
-
 		<sub class="sub-header" data-sitename="<?php echo $spine_main_header_values['site_name']; ?>" data-pagetitle="<?php echo $spine_main_header_values['page_title']; ?>" data-posttitle="<?php echo $spine_main_header_values['post_title']; ?>" data-default="<?php echo esc_html($spine_main_header_values['sub_header_default']); ?>" data-alternate="<?php echo esc_html($spine_main_header_values['sub_header_alternate']); ?>"><span class="sub-header-default"><?php echo strip_tags( $spine_main_header_values['sub_header_default'], '<a>' ); ?></span></sub>
-
-</header>
-
-<?php endif; ?>
