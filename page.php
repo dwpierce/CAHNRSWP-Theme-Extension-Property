e3668e1bd9c825feaa59abe08e9b0aac3aebe83a
Phil Cable <pcable@wsu.edu>
2015-11-12 13:03:08 -0800

    Page title condition revision

--- page.php
+++ page.php
@@ -7,12 +7,13 @@
 
 		<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
+			<?php $hide_title = get_post_meta( get_the_ID(), '_cahnrswp_hide_title', true ); ?>
+
       <?php if ( class_exists( 'CWP_Pagebuilder' ) && has_shortcode( get_the_content(), 'row' ) ) : ?>
 
 			<div id="page-<?php the_ID(); ?>" <?php post_class( 'builder-layout' ); ?>>
 
-				<?php $hide_title = get_post_meta( get_the_ID(), '_cahnrswp_hide_title', true ); ?>
-				<?php if ( ( ! is_front_page() && ! has_post_thumbnail() ) || '1' !== $hide_title ) : ?>
+				<?php if ( ! ( is_front_page() && has_post_thumbnail() ) && ! $hide_title ) : ?>
 				<header class="article-header">
 					<h1 class="article-title"><?php the_title(); ?></h1>
 				</header>
@@ -29,7 +30,7 @@
 				<div class="column one">
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<?php if ( ( ! is_front_page() && ! has_post_thumbnail() ) || '1' !== $hide_title ) : ?>
+						<?php if ( ! ( is_front_page() && has_post_thumbnail() ) && ! $hide_title ) : ?>
 						<header class="article-header">
 							<h1 class="article-title"><?php the_title(); ?></h1>
 						</header>
